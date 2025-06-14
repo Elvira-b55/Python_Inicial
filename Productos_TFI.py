a5775927cd3cfc254f80c7cb90d1334337dc158f
Elvira <elvira.programmer@gmail.com>
2025-06-14 08:12:06 -0300

    Primer proyecto de python inicial

---
+++ Productos_TFI.py
@@ -0,0 +1,17 @@
+__author__ = "Elvira Fernández"
+
+productos = []  #lista vacía para almacenar productos ingresados
+
+
+#ingreso de datos de productos nombre, categoria,precio(sin centavos)
+
+#visualizacion de productos registrados
+
+
+#busqueda de productos
+
+#eliminacion de productos
+
+#salir del programa
+
+
